10b27347280df0696abf1c26b1b6197baf8ecab4
satorunakamatsu <satoru.nakamatsu@gmail.com>
2023-12-09 19:07:16 +0900

    render deploy

--- kintaiproject/settings.py
+++ kintaiproject/settings.py
@@ -145,8 +145,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
-# if not DEBUG:
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
